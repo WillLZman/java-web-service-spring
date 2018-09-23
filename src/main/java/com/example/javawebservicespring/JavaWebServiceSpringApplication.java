2eb3f88626a21b887c17f779f7761cb269159161
Will <will.l.zimmerman@gmail.com>
2018-09-23 00:31:12 -0400

    init commit

---
+++ src/main/java/com/example/javawebservicespring/JavaWebServiceSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.javawebservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebServiceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebServiceSpringApplication.class, args);
+    }
+}
